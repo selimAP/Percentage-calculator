0af184a091515899935b7234ac451ccf7d6efe3e
selim <Selim3Boss@gmail.com>
2022-07-31 01:54:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.clear
+document.getElementById('myButton').onclick = function(){
+
+    var prozents = document.getElementById('prozentsatz').value;
+    var prozentw = document.getElementById('prozentwert').value;
+
+    document.querySelector('#grundwert').innerText = prozents * prozentw / 100
+}
+
+
+
+
+
+
+
+
