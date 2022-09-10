90a020b96c55f79cc5598769551dd029d8d44388
selim <Selim3Boss@gmail.com>
2022-09-11 01:25:35 +0200

    Little changes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('myButton').onclick = function(){
     var prozents = document.getElementById('prozentsatz').value;
     var prozentw = document.getElementById('prozentwert').value;
 
-    document.querySelector('#grundwert').innerText = prozents * prozentw / 100
+    document.querySelector('#grundwert').innerText = prozents * prozentw / 100;
 }
 
 
