6d98531deca6fbf5655a99390b9612a5b66341fd
Selim Afat <selimafat@outlook.de>
2022-12-09 10:10:03 +0100

    JavaScript code scaled and improved

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-console.clear
 document.getElementById('myButton').onclick = function(){
+    let prozents = document.getElementById("prozentsatz").value;
+    let prozentw = document.getElementById("prozentwert").value;
 
-    var prozents = document.getElementById('prozentsatz').value;
-    var prozentw = document.getElementById('prozentwert').value;
+    if (isNaN(prozents) || isNaN(prozentw)){
+        document.querySelector("#grundwert").innerHTML = "Enter a number!";
+        return;
+    }
 
-    document.querySelector('#grundwert').innerText = prozents * prozentw / 100;
+    document.querySelector("#grundwert").innerText = prozents * prozentw / 100 + "%";
 }
